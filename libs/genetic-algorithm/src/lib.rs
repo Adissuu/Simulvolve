38ab5979f3270b62e4e9b28aeb8bf1368e3d5a75
Adissuu <gabrieldubois.eng@gmail.com>
2024-02-18 15:03:51 -0500

    god classes but I'll clean up after

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,7 +1,83 @@
+use rand::seq::SliceRandom;
+use rand::Rng;
+use rand::RngCore;
+use std::iter::FromIterator;
+
 pub struct GeneticAlgorithm;
+pub struct RouletteWheelSelection;
 
 impl GeneticAlgorithm {
     pub fn new() -> Self {
         Self
+    }
+
+    pub fn evolve<I>(&self, population: &[I]) -> Vec<I> {
+        assert!(population.len() > 1);
+
+        (0..population.len())
+            .map(|_| {
+                // TODO selection
+                // TODO crossover
+                // TODO mutation
+                todo!()
+            })
+            .collect()
+    }
+}
+
+impl RouletteWheelSelection {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl SelectionMethod for RouletteWheelSelection {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
+    where
+        I: Individual,
+    {
+        population
+            .choose_weighted(rng, |individual| individual.fitness())
+            .expect("got an empty population")
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+        let population = vec![
+            TestIndividual::new(1.0),
+            TestIndividual::new(2.0),
+            TestIndividual::new(3.0),
+            TestIndividual::new(4.0),
+        ];
+
+        let actual = BTreeMap::new();
+
+        assert!(actual.is_none());
+    }
+}
+
+#[cfg(test)]
+#[derive(Clone, Debug)]
+pub struct TestIndividual {
+    fitness: f32,
+}
+
+#[cfg(test)]
+impl TestIndividual {
+    pub fn new(&self) -> Self {
+        Self { fitness }
+    }
+}
+
+#[cfg(test)]
+impl Individual for TestIndividual {
+    fn fitness(&self) -> f32 {
+        self.fitness
     }
 }
