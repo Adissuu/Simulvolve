895e3518ae321552f36f64994dd1330491dfe964
Adissuu <gabrieldubois.eng@gmail.com>
2024-02-26 21:21:34 -0500

    initialized frontend

---
+++ libs/simulation/src/lib.rs
@@ -0,0 +1,24 @@
+pub use self::{animal::*, food::*, world::*};
+
+mod animal;
+mod food;
+mod world;
+
+use nalgebra as na;
+use rand::{Rng, RngCore};
+
+pub struct Simulation {
+    world: World,
+}
+
+impl Simulation {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            world: World::random(rng),
+        }
+    }
+
+    pub fn world(&self) -> &World {
+        &self.world
+    }
+}
