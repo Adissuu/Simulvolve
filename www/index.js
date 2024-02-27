80b403fa47962c00dd2b0df0bc77cec7d737bd01
Adissuu <gabrieldubois.eng@gmail.com>
2024-02-26 21:35:55 -0500

    remove www as repo

---
+++ www/index.js
@@ -0,0 +1,3 @@
+import * as sim from "lib-simulation-wasm";
+
+alert("Who's that dog? " + sim.can_i_pet_that_dawg() + "!");
---
+++ www/webpack.config.js
@@ -0,0 +1,25 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin(['index.html'])
+  ],
+  experiments: {
+    asyncWebAssembly: true, // based on async modules
+  },
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/,
+        type: 'webassembly/async',
+      },
+    ],
+  },
+};
